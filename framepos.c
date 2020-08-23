550402d5824c4d3976f50e5f2636c486a4b36f67
Dr-Noob <peibolms@gmail.com>
2020-08-23 11:43:54 +0200

    Replace deprecated functions by new ones. Add decoding parallelism. Fix YUV file write to file (it was wrong!). Add threshold to compare images. Add simple progress message

--- framepos.c
+++ framepos.c
@@ -14,6 +14,30 @@
 #include <assert.h>
 
 #define MAX_PIX_FMT_STR_LENGTH 10
+#define N_FRAMES_DEBUG       1000
+#define THRESHOLD             100
+
+void writePlane(FILE *f, uint8_t *data, ptrdiff_t linesize, int w, int h) {
+  for (int y = 0; y < h; y++) {
+    fwrite(data, 1, w, f);
+    data += linesize;
+  }
+}
+
+bool write_frame_yuv(AVFrame *f, char* output_path) {
+  FILE *fp = fopen(output_path, "wb");
+  if(!fp) return false;
+  
+  int size = av_image_get_buffer_size(f->format, f->width, f->height, 16);
+  
+  writePlane(fp, f->data[0], f->linesize[0], f->width, f->height);
+  writePlane(fp, f->data[1], f->linesize[1], f->width / 2, f->height / 2);
+  writePlane(fp, f->data[2], f->linesize[2], f->width / 2, f->height / 2);
+  
+  fclose(fp);
+  
+  return true;
+}
 
 // Naive images_equal (needs intrinsics to vectorice!)
 bool images_equal(AVFrame *f1, AVFrame *f2) {
@@ -21,7 +45,7 @@ bool images_equal(AVFrame *f1, AVFrame *f2) {
   uint8_t *img2 = f2->data[0];
   
   for(int i=0; i < f1->width * f1->height; i++) {
-    if(img1[i] != img2[i])return false;
+    if(abs(img1[i]-img2[i]) > THRESHOLD)return false;
   }
   
   return true;
@@ -44,11 +68,10 @@ AVFrame *read_frame_yuv(char* input_path, int w, int h) {
     return NULL;
   }
   
-  int n = avpicture_get_size(dst_pixfmt, w, h);
+  int n = av_image_get_buffer_size(dst_pixfmt, w, h, 32);
   
   buffer = av_malloc(n * sizeof(uint8_t));
-  avpicture_fill((AVPicture *)dst, buffer, dst_pixfmt, w, h);  
-  
+  av_image_fill_arrays(dst->data, dst->linesize, buffer, dst_pixfmt, w, h, 32);  
   
   if((ret = fread(dst->data[0], sizeof(uint8_t), n, fp)) < n) {
     printf("fread: Read %d, expected to read %d\n", ret, n); 
@@ -77,8 +100,7 @@ int main(int argc, char **argv) {
   char* image_path = argv[2];
   int image_width = atoi(argv[3]);
   int image_height = atoi(argv[4]);
-  
-  AVCodec *codec = NULL;
+    
   AVCodecContext *ctx= NULL;
   AVCodecParameters *origin_par = NULL;  
   AVFormatContext *fmt_ctx = NULL;  
@@ -94,10 +116,10 @@ int main(int argc, char **argv) {
   int video_stream;
   int got_frame = 0;
   int byte_buffer_size;
-  int i = 0;
   int current_frame = 0;
   int result;
   bool end = 0;  
+  int n_threads = 8;
   char* buf = malloc(sizeof(char) * MAX_PIX_FMT_STR_LENGTH);
  
   if ((result = avformat_open_input(&fmt_ctx, video_path, NULL, NULL)) < 0) {
@@ -116,13 +138,13 @@ int main(int argc, char **argv) {
   }
 
   origin_par = fmt_ctx->streams[video_stream]->codecpar;
-
-  if ((codec = avcodec_find_decoder(origin_par->codec_id)) == NULL) {
+  
+  if ((fmt_ctx->video_codec = avcodec_find_decoder(origin_par->codec_id)) == NULL) {
     av_log(NULL, AV_LOG_ERROR, "avcodec_find_decoder\n");
     return EXIT_FAILURE;
   }
 
-  if ((ctx = avcodec_alloc_context3(codec)) == NULL) {
+  if ((ctx = avcodec_alloc_context3(fmt_ctx->video_codec)) == NULL) {
     av_log(NULL, AV_LOG_ERROR, "avcodec_alloc_context3\n");
     return EXIT_FAILURE;
   }
@@ -131,8 +153,11 @@ int main(int argc, char **argv) {
     av_log(NULL, AV_LOG_ERROR, "avcodec_parameters_to_context\n");
     return EXIT_FAILURE;
   }
+  
+  ctx->thread_count = n_threads;
+  ctx->thread_type = FF_THREAD_FRAME; // FF_THREAD_FRAME seems to be better than FF_THREAD_SLICE
 
-  if ((result = avcodec_open2(ctx, codec, NULL)) < 0) {
+  if ((result = avcodec_open2(ctx, fmt_ctx->video_codec, NULL)) < 0) {
     av_log(ctx, AV_LOG_ERROR, "avcodec_open2\n");
     return EXIT_FAILURE;
   }
@@ -153,48 +178,50 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
   
+  printf("Using codec: %s\n", fmt_ctx->video_codec->name);
   av_get_pix_fmt_string(buf, MAX_PIX_FMT_STR_LENGTH, ctx->pix_fmt);
   printf("Input video PIX_FMT: %s\n", buf);
   if(ctx->pix_fmt != AV_PIX_FMT_YUV420P) printf("WARNING: %s was not designed to work with format different than yuv420p\n", argv[0]);
 
-  i = 0;
   av_init_packet(&pkt);
-  do {
-    if (!end) {
-      if (av_read_frame(fmt_ctx, &pkt) < 0) {
-        end = true;
+  
+  // https://stackoverflow.com/questions/44711921/ffmpeg-failed-to-call-avcodec-send-packet
+  while (av_read_frame(fmt_ctx, &pkt) >= 0) {
+    if (pkt.stream_index == video_stream) {
+      result = avcodec_send_packet(ctx, &pkt);
+      
+      if (result < 0 || result == AVERROR(EAGAIN) || result == AVERROR_EOF) {
+        av_log(NULL, AV_LOG_ERROR, "avcodec_send_packet\n");
+        return EXIT_FAILURE;
+      }
+        
+      while((result = avcodec_receive_frame(ctx, fr)) >= 0) {                                           
+        current_frame++; 
+        if(current_frame % N_FRAMES_DEBUG == 0) { 
+          printf("\r%d decoded frames...", current_frame);
+          fflush(stdout);
+        }
+        number_of_written_bytes = av_image_copy_to_buffer(byte_buffer, byte_buffer_size,
+                                 (const uint8_t* const *)fr->data, (const int*) fr->linesize,
+                                 ctx->pix_fmt, ctx->width, ctx->height, 1);
+        if (number_of_written_bytes < 0) {
+          av_log(NULL, AV_LOG_ERROR, "av_image_copy_to_buffer\n");
+          return EXIT_FAILURE;
+        }            
+        if(images_equal(fr, img)) {
+          printf("FRAME %d MATCHES\n", current_frame);                
+          return EXIT_SUCCESS;
+        }
+      }
+      
+      if(result != AVERROR(EAGAIN) && result != AVERROR_EOF) {          
+        av_log(NULL, AV_LOG_ERROR, "avcodec_receive_frame\n");  
+        return EXIT_FAILURE;
       }
     }
-    if (end) {
-        pkt.data = NULL;
-        pkt.size = 0;
-    }
-    if (pkt.stream_index == video_stream || end) {
-        got_frame = 0;
-        if (pkt.pts == AV_NOPTS_VALUE)
-            pkt.pts = pkt.dts = i;
-        result = avcodec_decode_video2(ctx, fr, &got_frame, &pkt);
-        if (result < 0) {
-            av_log(NULL, AV_LOG_ERROR, "Error decoding frame\n");
-            return result;
-        }
-        if (got_frame) {    
-            current_frame++; 
-            number_of_written_bytes = av_image_copy_to_buffer(byte_buffer, byte_buffer_size,
-                                    (const uint8_t* const *)fr->data, (const int*) fr->linesize,
-                                    ctx->pix_fmt, ctx->width, ctx->height, 1);
-            if (number_of_written_bytes < 0) {
-              av_log(NULL, AV_LOG_ERROR, "av_image_copy_to_buffer\n");
-              return EXIT_FAILURE;
-            }            
-            if(images_equal(fr, img))
-              printf("%d\n", current_frame);
-        }
-        av_packet_unref(&pkt);
-        av_init_packet(&pkt);
-    }
-    i++;
-  } while (!end || got_frame);
+    
+    av_packet_unref(&pkt);
+  }
 
   av_packet_unref(&pkt);
   av_frame_free(&fr);
