e2db906dd14e135165c8447b1bdee982a93a1096
Dr-Noob <peibolms@gmail.com>
2021-08-28 10:34:09 +0200

    Improve yuv_to_png.sh

--- yuv_to_png.sh
+++ yuv_to_png.sh
@@ -1,1 +1,13 @@
-convert -size 1920x1080 -sampling-factor 4:2:0 -depth 8 $1 $2
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo 'Usage: '$0' input.yuv'
+  exit 1
+fi
+
+input="$1"
+output=$(basename -- "$input")
+output="${output%.*}.png"
+
+convert -size 1920x1080 -sampling-factor 4:2:0 -depth 8 "$input" "$output"
